15ed83bf3c8f753527d61f345a42b2acf3340b23
AnetaJordanova <79541593+AnetaJordanova@users.noreply.github.com>
2021-02-28 21:23:17 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+    public static void seyHello(String name) {
+        System.out.println("Hello" +name);
+
+    }
+    public static void seyGoodbye(String name){
+        System.out.println("Good Bye" +name);
+    }
+}
